204f037c9c40884e40aec7f45ebd2ccc8b5536f6
xujiaji <jiajixu@qq.com>
2018-06-12 15:37:38 +0800

    learn mvvm example

---
+++ LearnMVVM/app/src/main/java/com/xujiaji/learnmvvm/viewmodel/ProjectListViewModel.java
@@ -0,0 +1,35 @@
+package com.xujiaji.learnmvvm.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.xujiaji.learnmvvm.service.model.Project;
+import com.xujiaji.learnmvvm.service.repository.NetRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+/**
+ * author: xujiaji
+ * created on: 2018/6/11 22:11
+ * description:
+ */
+public class ProjectListViewModel extends AndroidViewModel
+{
+    private final LiveData<List<Project>> projectListObservable;
+
+    @Inject
+    public ProjectListViewModel(@NonNull NetRepository netRepository, @NonNull Application application)
+    {
+        super(application);
+        projectListObservable = netRepository.getProjectList("xujiaji");
+    }
+
+    public LiveData<List<Project>> getProjectListObservable()
+    {
+        return projectListObservable;
+    }
+}
